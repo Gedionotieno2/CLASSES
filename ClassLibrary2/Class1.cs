7b92a2810121f83917b9f22b7ed24472950f3553
Preston Owuor <powuor@fintech-group.com>
2019-07-19 19:21:53 +0300

    Add project files.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        [Fact]
+        public void idNumbershouldbeprovided()
+        {
+            Employee employee = new Employee();
+            Assert.NotNull(employee.IdNumber);
+
+        }
+        public static void Main(string[]args)
+        {
+
+        }
+    }
+    class Employee
+    {
+        public string IdNumber { get; set; }
+    }
+}
